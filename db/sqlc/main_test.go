f7ad3901858486ca008169c3369d9750df2d8ad9
zakhaev43 <imransuet@gmail.com>
2024-05-12 22:14:47 +0200

    Added unit tests for transfer and entry

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,23 +7,23 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-
 )
 
 const (
-    dbDriver = "postgres"
-    dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:123@localhost:5400/root?sslmode=disable"
 )
+
 var testQueries *Queries
 
-func TestMain(m *testing.M){
+func TestMain(m *testing.M) {
 
 	conn, err := sql.Open(dbDriver, dbSource)
-    if err != nil {
-        log.Fatal("cannot connect to db:", err)
-    }
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
 
-    testQueries = New(conn)
+	testQueries = New(conn)
 
-    os.Exit(m.Run())
+	os.Exit(m.Run())
 }
