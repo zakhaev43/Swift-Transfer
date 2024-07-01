4c43f1990d9436657fce91337c98efe0e8e45d55
zakhaev43 <imransuet@gmail.com>
2024-07-01 22:29:02 +0200

    updated main_test file

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	dbDriver = "postgres"
-	dbSource = "postgresql://root:123@localhost:5400/root?sslmode=disable"
+	dbSource = "postgresql://root:123@localhost:5400/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
