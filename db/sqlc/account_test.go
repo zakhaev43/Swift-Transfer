e67ac3e86ec0cfbd48dcfae36f993d4bbe27cd2e
zakhaev43 <imransuet@gmail.com>
2024-07-07 11:37:28 +0200

    added createuser and getuser test for db package

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -10,38 +10,38 @@ import (
 	"github.com/zakhaev43/Simple-Bank/util"
 )
 
+func createRandomAccount(t *testing.T) Account {
 
-func createRandomAccount(t *testing.T) Account{
-
-	arg:= CreateAccountParams{
-		Owner: util.RandomOwner(),
-		Balance: util.RandomMoney(),
+	user := createRandomUser(t)
+	arg := CreateAccountParams{
+		Owner:    user.Username,
+		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
 	account, err := testQueries.CreateAccount(context.Background(), arg)
 
-    require.NoError(t, err)
-    require.NotEmpty(t, account)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
 	require.Equal(t, arg.Owner, account.Owner)
-    require.Equal(t, arg.Balance, account.Balance)
-    require.Equal(t, arg.Currency, account.Currency)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
 	require.NotZero(t, account.ID)
-    require.NotZero(t, account.CreatedAt)
+	require.NotZero(t, account.CreatedAt)
 
 	return account
 }
 
-func TestCreateAccount(t *testing.T){
+func TestCreateAccount(t *testing.T) {
 
 	createRandomAccount(t)
 }
 
-func TestGetAccount(t *testing.T){
+func TestGetAccount(t *testing.T) {
 
-	account1:=createRandomAccount(t)
-	account2, err:= testQueries.GetAccount(context.Background(), account1.ID)
-	require.NoError(t,err)
-	require.NotEmpty(t,account2)
+	account1 := createRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
 	require.Equal(t, account1.ID, account2.ID)
 	require.Equal(t, account1.Owner, account2.Owner)
 	require.Equal(t, account1.Balance, account2.Balance)
@@ -98,5 +98,3 @@ func TestListAccounts(t *testing.T) {
 		require.NotEmpty(t, account)
 	}
 }
-
-
--- util/random.go
+++ util/random.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 	"time"
@@ -43,4 +44,9 @@ func RandomCurrency() string {
 	currencies := []string{"EUR", "USD", "CAD"}
 	n := len(currencies)
 	return currencies[globalRand.Intn(n)]
+}
+
+func RandomEmail() string {
+
+	return fmt.Sprintf("%s@gmail.com", RandomString(7))
 }
