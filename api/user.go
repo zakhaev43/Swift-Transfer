49c1917f9acd5b35d319038e8ce14dc22fd47746
zakhaev43 <imransuet@gmail.com>
2024-07-07 22:20:30 +0200

    BugFix: AddUser APi bug fixed

--- api/user.go
+++ api/user.go
@@ -11,9 +11,9 @@ import (
 
 type createUserRequest struct {
 	Username string `json:"username" binding:"required,alphanum"`
-	Password string `json: "password" binding:"required,min=6"`
-	FullName string `json: "full_name" binding:"required"`
-	Email    string `json: "email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6"`
+	FullName string `json:"full_name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
 }
 
 type createUserResponse struct {
