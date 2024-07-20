c33efc62978aa2de1c7dd1f4137d6319ffc85a42
zakhaev43 <imransuet@gmail.com>
2024-07-20 23:25:26 +0200

    added unit tests for middleware

---
+++ api/api_test.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
+	db "github.com/zakhaev43/Swift-Transfer/db/sqlc"
+	"github.com/zakhaev43/Swift-Transfer/util"
+)
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
+}
+
+func TestMain(m *testing.M) {
+	gin.SetMode(gin.TestMode)
+
+	os.Exit(m.Run())
+}
