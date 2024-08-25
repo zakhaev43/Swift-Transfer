d4e239b6aa93fb39bfdf58252f3b3da744c4bd11
zakhaev43 <imransuet@gmail.com>
2024-08-25 21:14:09 +0200

    start.sh file updated

--- start.sh
+++ start.sh
@@ -3,6 +3,12 @@
 set -e
 
 echo "run db migration"
+. /app/app.env
+
+# Debugging: Print environment variables
+echo "DB_DRIVER: $DB_DRIVER"
+echo "DB_SOURCE: $DB_SOURCE"
+
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
